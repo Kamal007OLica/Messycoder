48d5f299d5573a951b80f371b4c1ffaba5433dda
Kamalraaj Senthilkumar <160602717+Kamal007OLica@users.noreply.github.com>
2025-02-10 09:45:27 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+
+x = get_int("what's x? ")
+y = get_int("what's y? ")
+
+if x < y:
+      print('y is greater than x')
+elif x > y:
+    print('s is greater than y')
+else:
+    print('x is equal to y')
+
