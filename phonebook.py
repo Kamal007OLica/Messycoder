07867a988c6e666447388dcda2107c3fb0e40369
Kamalraaj Senthilkumar <160602717+Kamal007OLica@users.noreply.github.com>
2025-02-10 12:51:41 +0530

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,11 @@
+names = ['Yulia', 'David', 'John']
+
+name = input('name: ').upper
+
+
+for n in names:
+    if name == n:
+        print("found")
+        break
+    elif
+        print("not found")
