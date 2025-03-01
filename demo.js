f1c701879b33b421ffbdf5f8d46f9435f5344437
Kamalraaj Senthilkumar <160602717+Kamal007OLica@users.noreply.github.com>
2025-03-01 21:11:33 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+function factor()
+{
+    console.log("Favorite Actor: Kamal")
+}
+
+function fplayer()
+{
+    console.log("favorite Player: Dhoni")
+}
+
+function fmovie()
+{
+    console.log("Favorite Movie: Kadaisi Vivasyi")
+}
+
+
+factor()
+fplayer()
+fmovie()
